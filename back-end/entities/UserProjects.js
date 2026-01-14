956a63dfb9dfb8b429d32f50ab7b1b47e3e9a25e
leylanyzamova <nyzamovaleyla23@stud.ase.ro>
2026-01-14 20:59:58 +0200

    Fix UserProjects composite primary key

--- back-end/entities/UserProjects.js
+++ back-end/entities/UserProjects.js
@@ -1,15 +1,23 @@
 import db from "../dbConfig.js";
 import Sequelize from "sequelize";
 
-const UserProject = db.define("UserProjects", {
-  UserID: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
+const UserProject = db.define(
+  "UserProjects",
+  {
+    UserID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    ProjectID: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
   },
-  ProjectID: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 export default UserProject;
