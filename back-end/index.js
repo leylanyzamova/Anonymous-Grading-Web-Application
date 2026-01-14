06f4f6143761fc319de98530d7f467d6acfaaa75
leylanyzamova <nyzamovaleyla23@stud.ase.ro>
2026-01-14 17:48:35 +0200

    Fix CORS and expose auth routes for frontend

--- back-end/index.js
+++ back-end/index.js
@@ -20,10 +20,12 @@ app.use(
       "http://localhost:3000",
       "https://anonymous-grading-web-application-1.onrender.com"
     ],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
 );
+
 
 app.use(express.json());
 app.use(
